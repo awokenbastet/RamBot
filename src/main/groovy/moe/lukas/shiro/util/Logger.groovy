9ab0d51fbac2ad99ccbe40dee3b71ee9e42a1984
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-14 02:47:53 +0200

    These are not the semicolons you are looking for :ghost:

--- src/main/groovy/moe/lukas/shiro/util/Logger.groovy
+++ src/main/groovy/moe/lukas/shiro/util/Logger.groovy
@@ -12,7 +12,7 @@ class Logger {
      * @param loggable A string to log
      */
     private static void log(String tag, String loggable) {
-        println("[${new Date().format('dd/MM/yy - HH:MM:SS')}] (${tag.toUpperCase()}) $loggable");
+        println("[${new Date().format('dd/MM/yy - HH:MM:SS')}] (${tag.toUpperCase()}) $loggable")
     }
 
     static def info = { log("info", it) }
