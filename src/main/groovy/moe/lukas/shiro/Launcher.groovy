7156e1b508dc74045a9f1b534730d548d3ed6ca5
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-14 18:43:08 +0200

    Commit workspace

--- src/main/groovy/moe/lukas/shiro/Launcher.groovy
+++ src/main/groovy/moe/lukas/shiro/Launcher.groovy
@@ -1,9 +1,13 @@
 package moe.lukas.shiro
 
 import moe.lukas.shiro.core.Core
+import moe.lukas.shiro.util.Logger
 
 class Launcher {
     public static void main(String[] args) {
-        Core.boot();
+        Logger.info("Running from ${System.getProperty("user.dir")}")
+        Logger.info("If this path is incorrect kill the program now and check your setup!")
+
+        Core.boot()
     }
 }
