b1a007837d3c67c6b17a20419b42df4ede825512
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-21 15:02:57 +0200

    Add random game module

---
+++ shiro_modules/RandomGame.groovy
@@ -0,0 +1,52 @@
+import sx.blah.discord.handle.obj.Status
+import sx.blah.discord.api.IDiscordClient
+import moe.lukas.shiro.core.IAdvancedModule
+import moe.lukas.shiro.annotations.ShiroMeta
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+import static java.util.concurrent.TimeUnit.SECONDS
+
+@ShiroMeta(enabled = true, author = "sn0w")
+class RandomGame implements IAdvancedModule {
+    List<String> games = [
+        // phrases
+        "async is the future!",
+        "spoopy stuff",
+        "with senpai",
+        "ʕ•ᴥ•ʔ",
+        "༼ つ ◕_◕ ༽つ",
+        "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+        "✧ﾟ･: *ヽ(◕ヮ◕ヽ)",
+
+        // actual games
+        "HuniePop",
+        "Candy Crush",
+        "Hyperdimension Neptunia",
+        "Yu-Gi-Oh!",
+        "Pokémon",
+        "Final Fantasy",
+
+        // names
+        "with Shinobu-Chan",
+        "with Ako-Chan",
+        "with Nadeko",
+        "with Miku",
+        "with @Devsome",
+        "with @Serraniel",
+        "with you O_o",
+        "with cats",
+        "with JOHN CENA"
+    ]
+
+    void init(IDiscordClient client) {
+        new Thread({
+            while (true) {
+                client.changeStatus(Status.game(games[new Random().nextInt(games.size())]))
+                System.gc()
+                SECONDS.sleep(10)
+            }
+        }).start()
+    }
+
+    void action(MessageReceivedEvent e) {}
+}
