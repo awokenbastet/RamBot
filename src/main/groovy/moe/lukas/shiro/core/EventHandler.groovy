9d06c8a6055219be816ee62a1044549d2b5010ea
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-21 19:44:27 +0200

    Integrate cleverbot

--- src/main/groovy/moe/lukas/shiro/core/EventHandler.groovy
+++ src/main/groovy/moe/lukas/shiro/core/EventHandler.groovy
@@ -1,13 +1,21 @@
 package moe.lukas.shiro.core
 
 import moe.lukas.shiro.util.Logger
+import sx.blah.discord.handle.obj.IChannel
 import moe.lukas.shiro.annotations.ShiroCommand
+import com.google.code.chatterbotapi.ChatterBot
 import sx.blah.discord.api.events.EventSubscriber
+import com.google.code.chatterbotapi.ChatterBotType
 import sx.blah.discord.handle.impl.events.ReadyEvent
+import com.google.code.chatterbotapi.ChatterBotFactory
+import com.google.code.chatterbotapi.ChatterBotSession
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent
 import sx.blah.discord.handle.impl.events.DiscordDisconnectedEvent
 
 class EventHandler {
+    private ChatterBot cleverbot = new ChatterBotFactory().create(ChatterBotType.CLEVERBOT)
+    private LinkedHashMap<String, ChatterBotSession> cleverbotSessions = []
+
     @EventSubscriber
     @SuppressWarnings(["GrMethodMayBeStatic", "GroovyUnusedDeclaration"])
     void onReadyEvent(ReadyEvent e) {
@@ -27,7 +35,10 @@ class EventHandler {
             /**
              * Show help on [prefix]h / [prefix]help
              */
-            if (e.getMessage().getContent().matches(/^${Core.getPrefixForServer(e)}(help|h)/)) {
+            if (
+            e.getMessage().getContent().matches(/^${Core.getPrefixForServer(e)}(help|h)/) ||
+                e.getMessage().getContent().matches(/^<@${e.getClient().getOurUser().getID()}>\s(help|h)$/)
+            ) {
                 String message = ""
 
                 message += "Hi :3 \n"
@@ -49,7 +60,7 @@ class EventHandler {
                             message += "\t **${Core.getPrefixForServer(e)}${it.command()}**"
 
                             if (it.usage() != "") {
-                                message += " - ${it.usage()}"
+                                message += " | Usage: `${Core.getPrefixForServer(e)}${it.command()} ${it.usage()}`"
                             }
 
                             message += "\n"
@@ -64,16 +75,52 @@ class EventHandler {
                 /**
                  * Catch all other commands
                  */
+                boolean answered = false
+
                 ModuleLoader.modules.each { LinkedHashMap module ->
                     if (module.properties.enabled == true) {
+                        // Check for matches
                         module.properties.commands.any { ShiroCommand it ->
                             if (e.getMessage().getContent().matches(
                                 /^${Core.getPrefixForServer(e)}${it.command()}.*/
                             )) {
                                 GroovyObject object = module["class"].newInstance()
                                 object.invokeMethod("action", e)
+                                System.gc()
+
+                                answered = true
 
                                 // break loop
+                                return true
+                            }
+                        }
+                    }
+                }
+
+                if (!answered) {
+                    if (e.getMessage().getMentions().size() > 0) {
+                        e.getMessage().getMentions().any {
+                            if (it.getID() == e.getClient().getOurUser().getID()) {
+                                /**
+                                 * Forward anything else to cleverbot
+                                 */
+                                IChannel channel = e.getMessage().getChannel()
+
+                                try {
+                                    channel.toggleTypingStatus()
+                                } catch (Exception ex) {
+                                }
+
+                                cleverbotSessions[channel.getID()] = cleverbot.createSession(Locale.ENGLISH)
+                                String response = cleverbotSessions[channel.getID()].think(e.getMessage().getContent())
+
+                                try {
+                                    !channel.getTypingStatus() ?: channel.toggleTypingStatus()
+                                } catch (Exception ex) {
+                                }
+
+
+                                channel.sendMessage(":robot: " + response)
                                 return true
                             }
                         }
