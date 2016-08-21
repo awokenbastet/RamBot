1a051ea04dd88f2daa335d569667f7bdc7b051a5
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-21 17:49:25 +0200

    add !ping

---
+++ shiro_modules/Ping.groovy
@@ -0,0 +1,19 @@
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.core.IModule
+import moe.lukas.shiro.annotations.ShiroMeta
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+import sx.blah.discord.handle.obj.IMessage
+
+@ShiroMeta(
+    enabled = true,
+    author = "sn0w",
+    description = "Test my reflexes c:",
+    commands = [@ShiroCommand(command = "ping")]
+)
+class Ping implements IModule {
+    void action(MessageReceivedEvent e) {
+        long start = System.nanoTime()
+        IMessage message = e.getMessage().getChannel().sendMessage(":ping_pong: Pong! :grin:")
+        message.edit(message.content + " (${(System.nanoTime() - start) / 1000000}ms RTT)")
+    }
+}
