f86ec9a62e4dfe2df3fd99e78511e27beffb27cb
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-23 23:18:21 +0200

    Add minecraft module

---
+++ src/main/groovy/moe/lukas/shiro/modules/Minecraft.groovy
@@ -0,0 +1,38 @@
+package moe.lukas.shiro.modules
+
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.Unirest
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+@ShiroMeta(
+    enabled = true,
+    description = "Get a minecraft avatar",
+    author = "sn0w",
+    commands = [
+        @ShiroCommand(command = "mc", usage = "<user>"),
+        @ShiroCommand(command = "minecraft", usage = "<user>")
+    ]
+)
+class Minecraft implements IModule {
+    void action(MessageReceivedEvent e) {
+        String[] parts = e.getMessage().getContent().split(" ")
+
+        if (parts.size() < 2) {
+            e.getMessage().getChannel().sendMessage("Error :frowning:")
+            return
+        }
+
+        HttpResponse<InputStream> stream = Unirest.get("https://minotar.net/body/${parts[1]}/300.png").asBinary()
+
+        if (stream.getStatus() == 200) {
+            e.getMessage().getChannel().sendFile(stream.getBody(), "avatar.png")
+            System.gc()
+            return
+        }
+
+        e.getMessage().getChannel().sendMessage("Error :frowning:")
+    }
+}
