3e4d0bc5a27b00386fb021100f74d264b641cf04
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-24 22:43:26 +0200

    Add !random | Closes #14

---
+++ src/main/groovy/moe/lukas/shiro/modules/Roll.groovy
@@ -0,0 +1,33 @@
+package moe.lukas.shiro.modules
+
+import java.util.concurrent.ThreadLocalRandom
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+import sx.blah.discord.handle.obj.IChannel
+
+@ShiroMeta(
+    enabled = true,
+    description = "Roll a random number",
+    author = "sn0w",
+    commands = [
+        @ShiroCommand(command = "roll", usage = "<min> <max>")
+    ]
+)
+class Roll implements IModule {
+    void action(MessageReceivedEvent e) {
+        IChannel channel = e.getMessage().getChannel()
+        String[] parts = e.getMessage().getContent().split(" ")
+
+        if (parts.size() == 3) {
+            Random random = new Random()
+            channel.sendMessage(
+                ":crystal_ball: " +
+                    ThreadLocalRandom.current().nextInt(parts[1].toInteger(), parts[2].toInteger() + 1)
+            )
+        } else {
+            channel.sendMessage("Seems like you made a typo :frowning:")
+        }
+    }
+}
