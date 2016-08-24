deaf6df1e4deb9515b355606b347e4553974d65b
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-24 23:16:08 +0200

    Add random.cat | Closes #50

---
+++ src/main/groovy/moe/lukas/shiro/modules/RandomCat.groovy
@@ -0,0 +1,32 @@
+package moe.lukas.shiro.modules
+
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
+import com.mashape.unirest.http.Unirest
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+@ShiroMeta(
+    enabled = true,
+    author = "sn0w",
+    description = "Random cat images",
+    commands = [
+        @ShiroCommand(command = "cat")
+    ]
+)
+class RandomCat implements IModule {
+    void action(MessageReceivedEvent e) {
+        HttpResponse<JsonNode> response = Unirest.get("http://random.cat/meow").asJson()
+        if (response.getStatus() != 200) {
+            e.getMessage().getChannel().sendMessage("Error :crying_cat_face:")
+            return
+        }
+
+        e.getMessage().getChannel().sendMessage(
+            "MEOW! :smiley_cat: \n " +
+                response.getBody().getObject().getString("file")
+        )
+    }
+}
