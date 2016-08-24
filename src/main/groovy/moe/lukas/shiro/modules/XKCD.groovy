3a355d1821a12afcef9bd50e79bfef6afa15f056
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-24 22:59:32 +0200

    Add XKCD comics | Closes #36

---
+++ src/main/groovy/moe/lukas/shiro/modules/XKCD.groovy
@@ -0,0 +1,41 @@
+package moe.lukas.shiro.modules
+
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
+import com.mashape.unirest.http.Unirest
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import org.json.JSONObject
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+import sx.blah.discord.handle.obj.IChannel
+
+@ShiroMeta(
+    enabled = true,
+    description = "Get the latest XKCD comic",
+    author = "sn0w",
+    commands = [
+        @ShiroCommand(command = "xkcd")
+    ]
+)
+class XKCD implements IModule {
+    void action(MessageReceivedEvent e) {
+        IChannel channel = e.getMessage().getChannel()
+
+        HttpResponse<JsonNode> response = Unirest.get("https://xkcd.com/info.0.json").asJson()
+        if (response.getStatus() != 200) {
+            channel.sendMessage("Error :frowning:")
+            return
+        }
+
+        JSONObject object = response.getBody().getObject()
+        String msg = ""
+
+        msg += "${object.getInt("num")} from ${object.getString("day")}/${object.getString("month")}/${object.getString("year")}\n"
+        msg += object.getString("title") + "\n"
+        msg += object.getString("img") + "\n"
+        msg += object.getString("alt")
+
+        channel.sendMessage(msg)
+    }
+}
