b59e375aade47db48155f920b6f102842d0c3dd1
Lukas Breuer <lukas.breuer@outlook.com>
2016-08-28 17:40:28 +0200

    Add Avatar module | Closes #20

---
+++ src/main/groovy/moe/lukas/shiro/modules/Avatar.groovy
@@ -0,0 +1,27 @@
+package moe.lukas.shiro.modules
+
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+@ShiroMeta(
+    enabled = true,
+    description = "Get a user's avatar in big",
+    author = "sn0w",
+    commands = [
+        @ShiroCommand(command = "avatar", usage = "<@mention>")
+    ]
+)
+class Avatar implements IModule {
+    void action(MessageReceivedEvent e) {
+        if (e.message.mentions.size() > 0) {
+            e.message.channel.sendMessage(
+                "There you go! :grin: \n " +
+                    e.message.mentions[0].avatarURL
+            )
+        } else {
+            e.message.channel.sendMessage("You need to @mention someone!")
+        }
+    }
+}
