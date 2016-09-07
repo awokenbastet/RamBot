20af2ef58bfba668924dd7490bae9fec407c61c2
Lukas Breuer <lukas.breuer@outlook.com>
2016-09-08 00:46:33 +0200

    Add even more senseless random games

--- src/main/groovy/moe/lukas/shiro/modules/RandomGame.groovy
+++ src/main/groovy/moe/lukas/shiro/modules/RandomGame.groovy
@@ -12,29 +12,49 @@ class RandomGame implements IAdvancedModule {
     List<String> games = [
         // phrases
         "async is the future!",
+        "down with OOP!",
         "spoopy stuff",
-        "with senpai",
+
+        // Kaomoji
         "ʕ•ᴥ•ʔ",
         "༼ つ ◕_◕ ༽つ",
         "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
-        "✧ﾟ･: *ヽ(◕ヮ◕ヽ)",
+        "( ͡° ͜ʖ ͡°)",
+        "¯\\_(ツ)_/¯",
+        "(ง ͠° ͟ل͜ ͡°)ง",
+        "ಠ_ಠ",
+        "(╯°□°)╯︵ ʞooqǝɔɐɟ",
+        "♪~ ᕕ(ᐛ)ᕗ",
+        "\\ (•◡•) /",
+        "｡◕‿◕｡",
 
         // actual games
+        "Hearthstone",
         "Overwatch",
         "HuniePop",
         "Candy Crush",
         "Hyperdimension Neptunia",
         "Final Fantasy MCMX",
 
+        // software
+        "with FFMPEG",
+        "with libav",
+        "with groovy",
+        "with apache",
+        "with python",
+        "with Reflections",
+
         // names
         "with Shinobu-Chan",
         "with Ako-Chan",
         "with Nadeko",
         "with Miku",
-        "with @Serraniel",
         "with you O_o",
         "with cats",
-        "with JOHN CENA"
+        "with JOHN CENA",
+        "with senpai",
+        "with Serraniel#8978",
+        "with FADED#3237"
     ]
 
     void init(IDiscordClient client) {
