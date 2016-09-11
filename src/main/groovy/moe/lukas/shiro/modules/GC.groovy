689d5b835b4184aebcd2b5a590de37e60b0ca6c3
Lukas Breuer <lukas.breuer@outlook.com>
2016-09-11 22:19:10 +0200

    Add GC as last resort during memory spikes

---
+++ src/main/groovy/moe/lukas/shiro/modules/GC.groovy
@@ -0,0 +1,19 @@
+package moe.lukas.shiro.modules
+
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+
+@ShiroMeta(
+    enabled = true,
+    hidden = true,
+    commands = [@ShiroCommand(command = "gc", adminOnly = true)]
+)
+class GC implements IModule {
+    @Override
+    void action(MessageReceivedEvent e) {
+        System.gc()
+        e.message.channel.sendMessage(":wastebasket: Done!")
+    }
+}
