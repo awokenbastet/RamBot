e2b9577d474406478e2e7245531dadedf53e281b
Lukas Breuer <lukas.breuer@outlook.com>
2016-09-26 21:11:46 +0200

    Add rating module

---
+++ src/main/groovy/moe/lukas/shiro/modules/Ratings.groovy
@@ -0,0 +1,81 @@
+package moe.lukas.shiro.modules
+
+import moe.lukas.shiro.annotations.ShiroCommand
+import moe.lukas.shiro.annotations.ShiroMeta
+import moe.lukas.shiro.core.IModule
+import moe.lukas.shiro.util.Database
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent
+import sx.blah.discord.handle.obj.IGuild
+import sx.blah.discord.handle.obj.IUser
+
+@ShiroMeta(
+    enabled = true,
+    description = "Rate users up/down :)",
+    commands = [
+        @ShiroCommand(command = "rate", usage = "<up|++|down|-->")
+    ]
+)
+class Ratings implements IModule {
+    @Override
+    void action(MessageReceivedEvent e) {
+        String[] parts = e.message.content.split(" ")
+
+        if (parts.size() == 3) {
+            switch (parts[1]) {
+                case "up":
+                case "++":
+                case "down":
+                case "--":
+                    int score = rate(e.message.mentions[0], e.message.guild, parts[1])
+                    e.message.channel.sendMessage("""
+`${e.message.mentions[0].name}` has been rated **${parts[1]}**.
+His score is now `$score`
+""")
+                    break
+
+                default:
+                    e.message.channel.sendMessage(":no_entry: You can only vote `up/down/++/--`")
+                    break
+            }
+
+        }
+    }
+
+    int rate(IUser user, IGuild guild, String direction) {
+        int score = 0
+
+        List q = Database.instance.query("SELECT `score` FROM `shiro`.`ratings` WHERE `user` = ? AND `guild` = ?", [
+            user.ID, guild.ID
+        ])
+
+        if (q[0]?.score == null) {
+            Database.instance.query("INSERT INTO `shiro`.`ratings` (`user`, `guild`, `score`) VALUES (?,?,?);", [
+                user.ID,
+                guild.ID,
+                0
+            ])
+        } else {
+            score = q[0]["score"] as int
+        }
+
+        switch (direction) {
+            case "up":
+            case "++":
+                score++
+                break
+
+            case "down":
+            case "--":
+                score--
+                break
+        }
+
+        Database.instance.query("UPDATE `shiro`.`ratings` SET `score`=? WHERE `user`=? AND `guild`=?;", [
+            score,
+            user.ID,
+            guild.ID
+        ])
+
+        return score
+    }
+}
