1ed702acaaf7116bc49f46612dc9416599a97da2
Lukas Breuer <lukas.breuer@outlook.com>
2016-09-27 16:39:31 +0200

    Move travis script

---
+++ after_success.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    docker login -u="$DU" -p="$DP"
+    docker build -t "sn0w/shiro:$TRAVIS_COMMIT" .
+    docker push "sn0w/shiro:$TRAVIS_COMMIT"
+
+    docker tag "sn0w/shiro:$TRAVIS_COMMIT" "sn0w/shiro:$TRAVIS_BRANCH"
+    docker push "sn0w/shiro:$TRAVIS_COMMIT"
+fi
